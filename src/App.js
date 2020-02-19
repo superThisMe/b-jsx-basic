0d42d74f9ed14a3121f42c6101c08b15ea7254b7
superthisme <apprentice91@gmail.com>
2020-02-19 18:18:12 +0900

    20-02-19

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "John Doe";
+  const style = {
+    backgroundColor: 'green',
+    color: 'red'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={ style }>Hello, React World !!!!!</h1>
+      <h1 style={ {
+        backgroundColor: 'gray',
+        color: 'blue'
+      } }>Hello { name } !!!!!</h1>
+      <div className="react">Styled Element !!!!!</div>
+      <span>TEST 1</span>
+      <br />
+      {/* JSX의 주석입니다. */}
+      <span
+        //여기에는 //주석을 사용할 수 있습니다.
+      >TEST 2</span>
+    </>
   );
+
 }
 
 export default App;
